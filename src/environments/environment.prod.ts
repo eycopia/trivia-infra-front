5aabeaf91ca2e435761b3ee0a6bb0d7bef1234a8
JCopia <eycopia@gmail.com>
2025-11-27 02:28:50 -0500

    cambia back

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://trivia-infra-back.vercel.app',
-    socketUrl: 'https://trivia-infra-back.vercel.app'
+    apiUrl: 'https://trivia-infra-back.onrender.com',
+    socketUrl: 'https://trivia-infra-back.onrender.com'
 };
